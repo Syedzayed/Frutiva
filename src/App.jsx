1434a02c577b501edac8d3481c5b0c86b1289335
syed zayed ahmed <syedzayedahmed2004@gmail.com>
2025-07-31 22:31:03 +0530

    Update: latest version of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-
 import Products from './pages/Products';
-
-import Contact from './pages/Contact';
 import Login from './pages/Login';
 import ProductDetail from './Sections/Productdetails';
 
@@ -15,7 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Products />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
         <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
